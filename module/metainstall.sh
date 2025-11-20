eedd9c4352465f67042139b56950b47ab9341168
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-20 15:18:02 +0800

    scripts/metainstall: call install_module

--- module/metainstall.sh
+++ module/metainstall.sh
@@ -3,7 +3,31 @@
 # meta-mm metainstall.sh
 ############################################
 
+export KSU_HAS_METAMODULE="true"
+export KSU_METAMODULE="meta-mm"
+
 # Main installation flow
 ui_print "- Using meta-mm metainstall"
+
+# undo_handle_partition
+# because ksu moves them e.g. MODDIR/system/product to MODDIR/product
+# this way we can support normal hierarchy that ksu breaks
+undo_handle_partition() {
+	partition_to_undo="$1"
+	if [ -L "$MODPATH/system/$partition_to_undo" ] && [ -d "$MODPATH/$partition_to_undo" ]; then
+		# ui_print "- undo handle_partition for /$partition_to_undo"
+		rm -f "$MODPATH/system/$partition_to_undo"
+		mv -f "$MODPATH/$partition_to_undo" "$MODPATH/system/$partition_to_undo"
+	fi
+}
+
+# call install function, this is important!
+install_module
+
+# Run for typical partitions
+undo_handle_partition vendor
+undo_handle_partition product
+undo_handle_partition system_ext
+undo_handle_partition odm
 
 ui_print "- Installation complete"
